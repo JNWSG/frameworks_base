SuperiorOS/android_frameworks_base

core: More infos uploaded to Memochō
Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
Change-Id: I88727f8801e99cb208fd6ff390636f61531b64e8

services/core/java/com/android/server/am/AppErrors.java
@@ -46,6 +46,7 @@ import android.os.Bundle;
 import android.os.Message;
 import android.os.Process;
 import android.os.SystemClock;
+import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.ArrayMap;
@@ -643,9 +644,13 @@ class AppErrors {
             }
 
             // Add paste content for Memochō option
+            String devfp = SystemProperties.get("ro.vendor.build.fingerprint", "");
+            String superiorVers = SystemProperties.get("ro.superior.version", "");
             data.paste = "time: " + timeMillis + "\n" +
-                    "msg: " + longMsg + "\n" +
-                    "stacktrace: " + stackTrace;
+            "device fp:" + devfp + "\n" +
+            "superior vers:" + superiorVers + "\n" +
+            "msg: " + longMsg + "\n" +
+            "stacktrace: " + stackTrace;
 
             final Message msg = Message.obtain();
             msg.what = ActivityManagerService.SHOW_ERROR_UI_MSG;

JNWSG/frameworks_base

Signed-off-by: S I P U N <sipunkumar85@gmail.com>
Signed-off-by: Ghosuto <clash.raja10@gmail.com>

services/core/java/com/android/server/am/AppErrors.java
@@ -47,6 +47,7 @@ import android.os.Bundle;
 import android.os.Message;
 import android.os.Process;
 import android.os.SystemClock;
+import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.provider.Settings;
@@ -679,9 +680,13 @@ class AppErrors {
             }
 
             // Add paste content for Memochō option
+            String devfp = SystemProperties.get("ro.vendor.build.fingerprint", "");
+            String everestVers = SystemProperties.get("ro.everest.version", "");
             data.paste = "time: " + timeMillis + "\n" +
-                    "msg: " + longMsg + "\n" +
-                    "stacktrace: " + stackTrace;
+            "device fp:" + devfp + "\n" +
+            "everest vers:" + everestVers + "\n" +
+            "msg: " + longMsg + "\n" +
+            "stacktrace: " + stackTrace;
 
             final Message msg = Message.obtain();
             msg.what = ActivityManagerService.SHOW_ERROR_UI_MSG;